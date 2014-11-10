acdf7c249a024057fb9aea3895882f555471c775
yylyyl <yangyiliang@gmail.com>
2014-11-10 22:41:56 +0800

    Initial Commit

---
+++ yMessage/SecondViewController.h
@@ -0,0 +1,15 @@
+//
+//  SecondViewController.h
+//  yMessage
+//
+//  Created by yangyiliang on 14/11/10.
+//  Copyright (c) 2014年 yylyyl. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SecondViewController : UIViewController
+
+
+@end
+
---
+++ yMessage/SecondViewController.m
@@ -0,0 +1,27 @@
+//
+//  SecondViewController.m
+//  yMessage
+//
+//  Created by yangyiliang on 14/11/10.
+//  Copyright (c) 2014年 yylyyl. All rights reserved.
+//
+
+#import "SecondViewController.h"
+
+@interface SecondViewController ()
+
+@end
+
+@implementation SecondViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
